344de14d0694cd09f081f6a69b169920fc7a3695
Timur <timgizzat@gmail>
2025-08-07 23:09:57 +0500

    added the meme page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
-export {HomePage} from './HomePage/HomePage.tsx'
-export {AuthPage} from './AuthPage/AuthPage.tsx'
+export { HomePage } from './HomePage/HomePage.tsx';
+export { AuthPage } from './AuthPage/AuthPage.tsx';
+export { MemePage } from './MemePage/MemePage.tsx';
